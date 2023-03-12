02a5e9f4941735e7590e830e4f7ac616e98ef22e
zhengrongfei <105427287+zhengrongfei@users.noreply.github.com>
2023-03-12 22:50:49 +0800

    checkstyle 414->390 google

--- src/OnlineCoursesAnalyzer.java
+++ src/OnlineCoursesAnalyzer.java
@@ -17,22 +17,25 @@ import java.util.stream.Stream;
 public class OnlineCoursesAnalyzer {
 
     List<Course> courses = new ArrayList<>();
-
     public OnlineCoursesAnalyzer(String datasetPath) {
         BufferedReader br = null;
         String line;
         try {
+            
             br = new BufferedReader(new FileReader(datasetPath, StandardCharsets.UTF_8));
             br.readLine();
             while ((line = br.readLine()) != null) {
                 String[] info = line.split(",(?=([^\\\"]*\\\"[^\\\"]*\\\")*[^\\\"]*$)", -1);
                 Course course = new Course(info[0], info[1], new Date(info[2]), info[3], info[4], info[5],
-                        Integer.parseInt(info[6]), Integer.parseInt(info[7]), Integer.parseInt(info[8]),
-                        Integer.parseInt(info[9]), Integer.parseInt(info[10]), Double.parseDouble(info[11]),
-                        Double.parseDouble(info[12]), Double.parseDouble(info[13]), Double.parseDouble(info[14]),
-                        Double.parseDouble(info[15]), Double.parseDouble(info[16]), Double.parseDouble(info[17]),
-                        Double.parseDouble(info[18]), Double.parseDouble(info[19]), Double.parseDouble(info[20]),
-                        Double.parseDouble(info[21]), Double.parseDouble(info[22]));
+                        Integer.parseInt(info[6]), Integer.parseInt(info[7]), 
+                        Integer.parseInt(info[8]), Integer.parseInt(info[9]),
+                        Integer.parseInt(info[10]), Double.parseDouble(info[11]),
+                        Double.parseDouble(info[12]), Double.parseDouble(info[13]),
+                        Double.parseDouble(info[14]), Double.parseDouble(info[15]),
+                        Double.parseDouble(info[16]), Double.parseDouble(info[17]),
+                        Double.parseDouble(info[18]), Double.parseDouble(info[19]),
+                        Double.parseDouble(info[20]), Double.parseDouble(info[21]),
+                        Double.parseDouble(info[22]));
                 courses.add(course);
             }
         } catch (IOException e) {
@@ -176,8 +179,8 @@ public class OnlineCoursesAnalyzer {
 
         List<String> list =stream.sorted(
                 (c1,c2)->{
-                    double s1=(age-map3.get(c1.number))*(age-map3.get(c1.number))+(gender*100-map2.get(c1.number))*(gender*100-map2.get(c1.number))+(isBachelorOrHigher*100-map1.get(c1.number))*(isBachelorOrHigher*100-map1.get(c1.number));
-                    double s2=(age-map3.get(c2.number))*(age-map3.get(c2.number))+(gender*100-map2.get(c2.number))*(gender*100-map2.get(c2.number))+(isBachelorOrHigher*100-map1.get(c2.number))*(isBachelorOrHigher*100-map1.get(c2.number));
+                    double s1=(age-map3.get(c1.number))* (age- map3.get(c1.number))+ (gender*100- map2.get(c1.number))* (gender*100- map2.get(c1.number))+ (isBachelorOrHigher* 100-map1.get(c1.number))* (isBachelorOrHigher*100- map1.get(c1.number));
+                    double s2=(age-map3.get(c2.number))* (age- map3.get(c2.number))+ (gender*100- map2.get(c2.number))* (gender*100- map2.get(c2.number))+ (isBachelorOrHigher* 100-map1.get(c2.number))* (isBachelorOrHigher*100- map1.get(c2.number));
                     if((s1- s2) < 0){
                         return -1;
                     }else if ((s1 - s2) > 0){
